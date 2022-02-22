60f365cede554a6a34c9897d48ac00d5e9e8658d
Christian677 <chryzen9@gmail.com>
2022-02-22 19:05:28 +0100

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,23 +2,8 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
+    
+    <h1>Insert content here!</h1>
+    
 </div>
-<h1>ciao</h1>
 @endsection
--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,1 +1,4 @@
 {{-- create nav here --}}
+<div class="bg-indigo-600 p-10 w-full" style="background-color: rgb(241, 236, 236)">
+  <h3>insert nav here</h3>
+</div>
