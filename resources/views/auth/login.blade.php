052e3c3b8b125cd23dc77f4636db884f75b60f4e
axesol22 <alessiosolari22@gmail.com>
2022-02-23 16:27:23 +0100

    login page and db updates

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,8 +1,83 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+    <link href="{{ asset('css/access.css') }}" rel="stylesheet">
+</head>
+<body>
 
-@section('content')
-<div class="container mt-7">
-    <div class="row justify-content-center">
+    <div class="bg-img container overflow-hidden mt-7">
+
+        <div class="content overflow-hidden">
+          <header>Login</header>
+          <form action = "#">
+
+            <div class="field">
+              <span class="fa fa-user"></span>
+              <input type="email" class="@error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="Email o numero di telefono" >
+              @error('email')
+                    <span class="invalid-feedback" role="alert">
+                         <strong>{{ $message }}</strong>
+                    </span>
+              @enderror
+            </div>
+
+            <div class="field space">
+              <span class="fa fa-lock"></span>
+              <input id="password" type="password" class="pass-key form-control @error('password') is-invalid @enderror" name="password" required 
+              autocomplete="current-password" placeholder="Password">
+              @error('password')
+                 <span class="invalid-feedback" role="alert">
+                 <strong>{{ $message }}</strong>
+                 </span>
+              @enderror
+              <span class="show">SHOW</span>
+            </div>
+
+            <div class="pass">
+              <a href="#">Password dimenticata?</a>
+            </div>
+
+            <div class="field">
+              <input type="submit" value="LOGIN">
+            </div>
+
+          </form>
+          
+          <div class="login">Registrati con</div>
+          <div class="links">
+            <div class="facebook">
+              <a href="#"><i class="fab fa-facebook-f"><span>Facebook</span></i></a>
+            </div>
+            <div class="instagram">
+              <a href="#"><i class="fab fa-instagram"><span>Instagram</span></i></a>
+            </div>
+          </div>
+          <div class="signup">Non hai un account?
+            <a href="#">Registrati ora</a>
+          </div>
+        </div>
+  
+      <script>
+        const pass_field = document.querySelector('.pass-key');
+        const showBtn = document.querySelector('.show');
+        showBtn.addEventListener('click', function(){
+         if(pass_field.type === "password"){
+           pass_field.type = "text";
+           showBtn.textContent = "HIDE";
+           showBtn.style.color = "#3498db";
+         }else{
+           pass_field.type = "password";
+           showBtn.textContent = "SHOW";
+           showBtn.style.color = "#222";
+         }
+        });
+      </script>
+
+    {{-- <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Login') }}</div>
@@ -68,6 +143,9 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> --}}
 </div>
-@endsection
+    
+</body>
+</html>
+
